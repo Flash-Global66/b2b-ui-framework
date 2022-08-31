2ed2031b01ed3a1febdffa2dbf6525b2c1ea0d63
Andersson Mesa <andercard66gmail.com>
2022-08-30 23:16:24 -0500

    Se configuro provider

---
+++ components/ConfigProvider/ConfigProvider.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-config-provider
+    v-bind="{ ...$attrs, ...$props }"
+    namespace="g"
+  >
+    <!--
+      @slot customize default content
+    -->
+    <slot />
+  </el-config-provider>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { ElConfigProvider } from 'element-plus';
+import es from 'element-plus/dist/locale/es.mjs'
+
+// TYPES
+import { Language } from './types/locale';
+
+export default defineComponent({
+  name: 'ConfigProvider',
+  components: {
+    ElConfigProvider,
+  },
+  props: {
+    /**
+     * global Initial zIndex
+     */
+     zIndex: {
+      type: Number,
+      default: '-'
+    },
+    /**
+     * Locale Object
+     */
+     locale: {
+      type: Object as PropType<Language>,
+      default: es,
+    },
+  },
+});
+</script>
