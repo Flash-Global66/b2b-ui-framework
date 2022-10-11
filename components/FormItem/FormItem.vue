c7073a4a7e35356bbc271f94a51d639d29e1c96c
Andersson Mesa <andercard66@gmail.com>
2022-10-11 12:26:07 -0500

    Se modifico formItem

--- components/FormItem/FormItem.vue
+++ components/FormItem/FormItem.vue
@@ -2,14 +2,16 @@
   <el-form-item
     v-bind="$attrs"
   >
-    <template v-for="(_, name) in $slots" #[name]="slotData">
-      <slot :name="name" v-bind="slotData || {}" />
-    </template>
+  <template v-for="(_, name) in $slots" #[name]="slotData">
+    <slot :name="name" v-bind="slotData || {}" />
+  </template>
+
+  <slot name="append" />
   </el-form-item>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch, computed, PropType } from 'vue';
+import { defineComponent } from 'vue';
 import { ElFormItem } from 'element-plus';
 
 export default defineComponent({
@@ -27,51 +29,39 @@ export default defineComponent({
     /**
      * A key of model. It could be an array of property paths (e.g ['a', 'b', 0]). In the use of validate and resetFields method, the attribute is required.
      */
-     prop: {
-      type: String as PropType<string | string[]>,
-      default: ''
-    },
+    //  prop: {
+    //   type: String as PropType<string | string[]>,
+    //   default: ''
+    // },
     /**
       * Validation rules of form, see the following table, more advanced usage at async-validator.
     */
-    rules: {
-      type: Object,
-      default: () => ({})
-    },
+    // rules: {
+    //   type: Object,
+    //   default: () => ({})
+    // },
     /**
      * Field error message, set its value and the field will validate error and show this message immediately.
      */
-     error: {
-      type: String,
-      default: ''
-    },
+    //  error: {
+    //   type: String,
+    //   default: ''
+    // },
     /**
      * Whether to show the error message.
      */
-     showMessage: {
-      type: Boolean,
-      default: true
-    },
+    //  showMessage: {
+    //   type: Boolean,
+    //   default: true
+    // },
     /**
      * Whether to trigger validation when the rules prop is changed.
      */
-     size: {
-      type: String as PropType<string | string[]>,
-      default: 'default',
-    },
+    //  size: {
+    //   type: String as PropType<string | string[]>,
+    //   default: 'default',
+    // },
   },
-  setup(props, { emit, slots }) {
-    const refForm = ref();
-
-    watch(() => refForm.value, (rf) => {
-      console.log('que', rf);
-      // prefixWidth.value = rf.prefixWidth;
-    })
-
-    return {
-      refForm,
-    }
-  }
 });
 </script>
 
