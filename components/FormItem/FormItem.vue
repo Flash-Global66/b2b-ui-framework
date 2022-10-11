fad464c833fa26bf45f4850d350f576b67ca0247
Andersson Mesa <andercard66@gmail.com>
2022-10-11 12:42:27 -0500

    Se modifico form Item

--- components/FormItem/FormItem.vue
+++ components/FormItem/FormItem.vue
@@ -1,13 +1,15 @@
 <template>
-  <el-form-item
-    v-bind="$attrs"
-  >
-  <template v-for="(_, name) in $slots" #[name]="slotData">
-    <slot :name="name" v-bind="slotData || {}" />
-  </template>
-
-  <slot name="append" />
-  </el-form-item>
+  <section class="g-form-item-wrapper">
+    <el-form-item
+      v-bind="$attrs"
+    >
+      <template v-for="(_, name) in $slots" #[name]="slotData">
+        <slot :name="name" v-bind="slotData || {}" />
+      </template>
+  
+    </el-form-item>
+    <slot name="append" />
+  </section>
 </template>
 
 <script lang="ts">
@@ -25,43 +27,43 @@ export default defineComponent({
      */
     'resetField',
   ],
-  props: {
-    /**
-     * A key of model. It could be an array of property paths (e.g ['a', 'b', 0]). In the use of validate and resetFields method, the attribute is required.
-     */
-    //  prop: {
-    //   type: String as PropType<string | string[]>,
-    //   default: ''
-    // },
-    /**
-      * Validation rules of form, see the following table, more advanced usage at async-validator.
-    */
-    // rules: {
-    //   type: Object,
-    //   default: () => ({})
-    // },
-    /**
-     * Field error message, set its value and the field will validate error and show this message immediately.
-     */
-    //  error: {
-    //   type: String,
-    //   default: ''
-    // },
-    /**
-     * Whether to show the error message.
-     */
-    //  showMessage: {
-    //   type: Boolean,
-    //   default: true
-    // },
-    /**
-     * Whether to trigger validation when the rules prop is changed.
-     */
-    //  size: {
-    //   type: String as PropType<string | string[]>,
-    //   default: 'default',
-    // },
-  },
+  // props: {
+  //   /**
+  //    * A key of model. It could be an array of property paths (e.g ['a', 'b', 0]). In the use of validate and resetFields method, the attribute is required.
+  //    */
+  //   //  prop: {
+  //   //   type: String as PropType<string | string[]>,
+  //   //   default: ''
+  //   // },
+  //   /**
+  //     * Validation rules of form, see the following table, more advanced usage at async-validator.
+  //   */
+  //   // rules: {
+  //   //   type: Object,
+  //   //   default: () => ({})
+  //   // },
+  //   /**
+  //    * Field error message, set its value and the field will validate error and show this message immediately.
+  //    */
+  //   //  error: {
+  //   //   type: String,
+  //   //   default: ''
+  //   // },
+  //   /**
+  //    * Whether to show the error message.
+  //    */
+  //   //  showMessage: {
+  //   //   type: Boolean,
+  //   //   default: true
+  //   // },
+  //   /**
+  //    * Whether to trigger validation when the rules prop is changed.
+  //    */
+  //   //  size: {
+  //   //   type: String as PropType<string | string[]>,
+  //   //   default: 'default',
+  //   // },
+  // },
 });
 </script>
 
