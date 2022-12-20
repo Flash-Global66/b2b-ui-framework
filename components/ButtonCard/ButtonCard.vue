30e6b328a7b97072763d94b37afb3b184bb58e65
Andersson Mesa <andercard66@gmail.com>
2022-12-20 17:46:42 -0500

    update componente button card

--- components/ButtonCard/ButtonCard.vue
+++ components/ButtonCard/ButtonCard.vue
@@ -25,13 +25,9 @@ defineProps({
     type: Boolean,
     default: false,
   },
-  width: {
-    type: String,
-    default: 'max-w-[300px]'
-  },
   height: {
     type: String,
-    default: 'h-44'
+    default: ''
   },
 })
 
@@ -45,39 +41,40 @@ function onClick (event: Event) {
 
 <template>
   <section
-    :class="width"
-    class="w-full"
-    @click="onClick">
-    <div
-      class="rounded-xl flex flex-col justify-center items-center border-[1px] duration-200 group"
-      :class="[
-        disabled
-          ? 'cursor-not-allowed bg-transparent border-gray-7'
-          : 'cursor-pointer bg-white shadow-md border-transparent hover:border-blue-3',
-        height,
-      ]"
-    >
-      <slot name="image">
-        <div
-          :class="{ 'w-20 h-20 bg-gray-9 rounded-full flex justify-center items-center': iconRounded }">
-          <fa-icon class="text-blue-1 text-5xl duration-200 group-hover:text-gray-3" :icon="icon" />
-        </div>
-      </slot>
+    @click="onClick"
+    class="rounded-xl flex flex-col justify-center items-center border-[1px] duration-200 group"
+    :class="[
+      disabled
+        ? 'cursor-not-allowed bg-transparent border-gray-7'
+        : 'cursor-pointer bg-white shadow-md border-transparent hover:border-blue-3',
+      height.length ? height : 'min-h-[200px]',
+    ]"
+  >
+    <slot name="image">
+      <div :class="{ 'w-20 h-20 bg-gray-9 rounded-full flex justify-center items-center': iconRounded }">
+        <fa-icon class="text-blue-1 text-5xl duration-200 group-hover:text-gray-3" :icon="icon" />
+      </div>
+    </slot>
 
-      <slot name="title">
-        <p
-          :class="disabled ? 'text-gray-5' : 'text-blue-1'"
-          class="duration-200 text-gray-2 font-normal text-lg text-center w-4/5 mt-3 leading-6 group-hover:text-gray-3"
-          v-html="title"
-        />
-      </slot>
-    </div>
+    <slot name="title">
+      <h4
+        :class="disabled ? 'text-gray-5' : 'text-gray-2'"
+        class=" font-semibold text-xl leading-6 mb-2 text mt-5"
+        v-html="title"
+      />
+    </slot>
     <slot name="subtitle">
       <p
-        class="text-center font-normal text-sm mt-4"
-        :class="disabled ? 'text-gray-4' : 'text-gray-2'"
+        :class="disabled ? 'text-gray-5' : 'text-gray-2'"
+        class="leading-6 text"
         v-html="subtitle"
       />
     </slot>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.text {
+  @apply group-hover:text-gray-3 duration-200 w-4/5 text-center
+}
+</style>
