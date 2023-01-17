c879dc2041c65d722bb3b2c3e701fb82b3e51e4f
Andersson Mesa <andercard66@gmail.com>
2023-01-17 11:15:36 -0500

    Se agrego doc para input

--- stories/Input.stories.ts
+++ stories/Input.stories.ts
@@ -1,6 +1,7 @@
 import { Meta, StoryFn } from '@storybook/vue3';
 import { ref } from 'vue';
 import { action } from '@storybook/addon-actions';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // COMPONENTS
 import { GInput } from '../components/Input';
@@ -145,6 +146,84 @@ Clearable.parameters = {
   docs: {
     description: {
       story: `Make the Input clearable with the \`clearable\` attribute.`,
+    },
+  },
+};
+
+const TemplateSlots: StoryFn<typeof GInput> = (args, selected) => {
+  return {
+    components: { GInput, GConfigProvider, FontAwesomeIcon },
+    setup() {
+      const name = ref('');
+
+      return {
+        args,
+        name,
+        onKeydown: action('keydown'),
+      };
+    },
+    template: `
+      <g-config-provider>
+        <div class="grid grid-cols-1 gap-4">
+          <g-input
+            v-model="name"
+            label="Buscar"
+            @keydown="onKeydown"
+          >
+            <template #prefix>
+              <FontAwesomeIcon
+                :icon="['fas', 'search']"
+              />
+            </template>
+          </g-input>
+
+          <g-input
+            v-model="name"
+            label="Suscribirse"
+            @keydown="onKeydown"
+          >
+            <template #suffix>
+              <FontAwesomeIcon
+                :icon="['fas', 'arrow-right']"
+              />
+            </template>
+          </g-input>
+
+          <g-input
+            v-model="name"
+            @keydown="onKeydown"
+          >
+            <template #prefix>
+              <div class="pr-2">
+                Como estas?
+              </div>
+            </template>
+          </g-input>
+
+          <g-input
+            v-model="name"
+            label="Buscar..."
+            @keydown="onKeydown"
+          >
+            <template #suffix>
+              Enter
+            </template>
+          </g-input>
+        </div>
+      </g-config-provider>
+    `,
+  }
+};
+
+export const Slots = TemplateSlots.bind({});
+
+Slots.args = {
+};
+
+Slots.parameters = {
+  docs: {
+    description: {
+      story: `\`Prefix\`: Content as Input prefix, only works when type is not 'textarea' -  \`Suffix\`: Content as Input suffix, only works when type is not 'textarea.'`,
     },
   },
 };
