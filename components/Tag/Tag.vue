f18a328a6e7ee18cada6bcded34459bd53844e4d
Andersson Mesa <andercard66@gmail.com>
2023-07-11 23:21:41 -0500

    se agrego componente de tag

---
+++ components/Tag/Tag.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-tag
+    v-bind="{ ...$attrs, ...$props }"
+    @click="$emit('click')"
+    @close="$emit('close')"
+  >
+    <!--
+      @slot customize default content
+    -->
+    <slot />
+  </el-tag>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import { ElTag, componentSizes } from 'element-plus';
+
+export default defineComponent({
+  name: 'GTag',
+  components: {
+    ElTag,
+  },
+  props: {
+  /**
+   * @description type of Tag
+   */
+   type: {
+    type: String,
+    values: ['success', 'info', 'warning', 'danger', ''],
+    default: '',
+  },
+  /**
+   * @description whether Tag can be removed
+   */
+  closable: Boolean,
+  /**
+   * @description whether to disable animations
+   */
+  disableTransitions: Boolean,
+  /**
+   * @description whether Tag has a highlighted border
+   */
+  hit: Boolean,
+  /**
+   * @description background color of the Tag
+   */
+  color: {
+    type: String,
+    default: '',
+  },
+  /**
+   * @description size of Tag
+   */
+  size: {
+    type: String,
+    values: componentSizes,
+    default: '',
+  },
+  /**
+   * @description whether Tag is rounded
+   */
+  round: Boolean,
+  },
+  setup: () => {
+    
+    return;
+  },
+});
+</script>
---
+++ components/Tag/index.ts
@@ -0,0 +1,3 @@
+import GTag from "./Tag.vue";
+
+export { GTag };
