d35aaa51bd2d590090890cb3aa95fa9a92db861e
Andersson Mesa <andersson.mesa@global66.com>
2025-01-22 16:03:16 -0500

    avance segmented

--- stories/Segmented.stories.ts
+++ stories/Segmented.stories.ts
@@ -1,3 +1,4 @@
+import { ref } from "vue";
 import { StoryFn, Meta } from "@storybook/vue3";
 
 import { GSegmented } from '../components/Segmented';
@@ -12,13 +13,14 @@ const Template: StoryFn<typeof GSegmented> = (args) => ({
   components: { GSegmented, GConfigProvider },
   template: `
   <g-config-provider>
-    <g-segmented v-bind="args">
-      Hola
-    </g-segmented/>
+    <g-segmented v-model="value" :options="options" size="large"></g-segmented>
   </g-config-provider>
   `,
   setup() {
-    return { args };
+    const value = ref('Mon')
+
+    const options = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    return { args, options, value };
   }
 });
 
