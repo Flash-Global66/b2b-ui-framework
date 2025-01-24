d521cade80a6ff33b04dcb886b19cadc64eb9f34
Andersson Mesa <andersson.mesa@global66.com>
2025-01-23 22:11:49 -0500

    avance document icon and pill

---
+++ components/Icon/Icon.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+export default { name: 'IconCustom' };
+</script>
+
+<script lang="ts" setup>
+import { Icomoon } from "vue-icomoon";
+import iconSet from "./selection.json";
+import { PropType } from "vue";
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String as PropType<string | number>,
+    default: "1em",
+  },
+  color: String,
+  style: Object,
+  title: String,
+  class: {
+    type: [String, Object, Array] as PropType<string | Record<string, boolean> | Array<string | Record<string, boolean>>>,
+    default: "",
+  },
+  disableFill: Boolean,
+  removeInitialStyle: Boolean,
+});
+
+</script>
+
+<template>
+  <icomoon v-if="name" :iconSet="iconSet" v-bind="{ ...props }" />
+</template>
