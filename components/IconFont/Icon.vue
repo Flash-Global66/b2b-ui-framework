f2a77055245f325f3396074ebcf75e4279092ebc
Andersson Mesa <andersson.mesa@global66.com>
2025-01-30 23:19:27 -0500

    avance icon font

---
+++ components/IconFont/Icon.vue
@@ -0,0 +1,44 @@
+<template>
+  <font-awesome-icon v-if="selectedIcon" :icon="selectedIcon" />
+</template>
+
+<script lang="ts" setup>
+import { computed, PropType } from 'vue';
+import { debugWarn } from "element-plus/es/utils/index.mjs";;
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import type { IconName, IconString } from './types';
+import * as SolidPro from './src/fas-solid-pro';
+import * as SolidFree from './src/fas-solid-free';
+import * as RegularPro from './src/far-regular-pro';
+import * as LightPro from './src/fal-light-pro';
+import * as BrandsFree from './src/fab-free';
+
+// Registro de iconos
+library.add(
+  SolidPro,
+  SolidFree,
+  RegularPro,
+  LightPro,
+  BrandsFree
+);
+
+const props = defineProps({
+  name: {
+    type: String as PropType<IconString>,
+    required: true
+  }
+});
+
+const selectedIcon = computed(() => {
+  const [weight, iconName] = props.name.split(' ');
+  if (!iconName) {
+    console.error('Icon name is invalid');
+    debugWarn('IconFont', 'Icon name is invalid')
+    return;
+  };
+
+  return `fa-${weight} fa-${iconName}`;
+});
+</script>
