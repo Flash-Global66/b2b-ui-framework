f5b267bc829631d398f7da87bec6681735727d29
Andersson Mesa <andersson.mesa@global66.com>
2025-02-05 11:22:10 -0500

    update componente inline

--- components/Inline/Inline.vue
+++ components/Inline/Inline.vue
@@ -1,35 +1,3 @@
-<template>
-  <div
-    :class="inlineClass"
-    ref="inlineRef"
-    :aria-label="ariaLabel || 'inline'"
-  >
-    <div :class="[ns.e('icon')]">
-      <slot name="icon" />
-    </div>
-    <div class="">
-      <h3 :class="[ns.e('title')]"> {{ title }} </h3>
-      <p v-if="description" :class="[ns.e('description')]">
-        <slot>
-          {{ description }}
-        </slot>
-      </p>
-      <div v-if="links.length" role="group" :class="[ns.e('links')]">
-        <button
-          v-for="(link, idx) in links"
-          :key="idx"
-          @click="link.action" 
-          :class="[ns.e('link')]"
-          :aria-label="link.ariaLabel || link.label"
-          type="button"
-        >
-          {{ link.label }}
-        </button>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script lang="ts" setup>
 import {
   ButtonNativeType,
@@ -45,12 +13,15 @@ import {
   useSizeProp,
 } from "element-plus";
 import { computed, defineComponent, PropType, reactive, ref, watch } from "vue";
+import { GIconFont } from '@flash-global66/b2b-ui-icon-font';
 import { useActiveElement, useResizeObserver } from "@vueuse/core";
 import { inlineProps } from "./inline";
 import { isObject } from "element-plus/es/utils/types.mjs";
 import { debugWarn } from "element-plus/es/utils/index.mjs";;
 
 const props = defineProps(inlineProps)
+
+const visible = ref(true);
 
 const ns = useNamespace('inline')
 const inlineSize = useFormSize()
@@ -63,4 +34,50 @@ const inlineClass = computed(() => [
   ns.m(props.type)
 ])
 
+function onClose() {
+  visible.value = false;
+}
+
 </script>
+
+
+<template>
+  <div
+    v-if="visible"
+    :class="inlineClass"
+    ref="inlineRef"
+    :aria-label="ariaLabel || 'inline'"
+  >
+    <g-icon-font :class="[ns.e('icon')]" :name="icon" />
+    <div class="">
+      <h3 :class="[ns.e('title')]"> {{ title }} </h3>
+      <p v-if="description" :class="[ns.e('description')]">
+        <slot>
+          {{ description }}
+        </slot>
+      </p>
+      <div
+        v-if="links.length"
+        role="group"
+        :class="[ns.e('links')]"
+      >
+        <button
+          v-for="(link, idx) in links"
+          :key="idx"
+          @click="link.action" 
+          :class="[ns.e('link')]"
+          :aria-label="link.ariaLabel || link.label"
+          type="button"
+        >
+          {{ link.label }}
+        </button>
+      </div>
+    </div>
+    <g-icon-font
+      v-if="!hideClose"
+      @click="onClose"
+      :class="ns.e('close')"
+      name="regular times"
+    />
+  </div>
+</template>
