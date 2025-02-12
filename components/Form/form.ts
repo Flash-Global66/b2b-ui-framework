03da0efc0e716c5deb904fb4132df027c8113817
Andersson Mesa <andersson.mesa@global66.com>
2025-02-12 18:40:54 -0500

    add errors in input and form-item

--- components/Form/form.ts
+++ components/Form/form.ts
@@ -1,4 +1,3 @@
-import { componentSizes } from 'element-plus'
 import {
   buildProps,
   definePropType,
@@ -12,13 +11,6 @@ import type { FormItemProp } from './form-item'
 import type { FormRules } from './types'
 
 export const formMetaProps = buildProps({
-  /**
-   * @description Control the size of components in this form.
-   */
-  size: {
-    type: String,
-    values: componentSizes,
-  },
   /**
    * @description Whether to disable all components in this form. If set to `true`, it will override the `disabled` prop of the inner component.
    */
@@ -38,54 +30,8 @@ export const formProps = buildProps({
     type: definePropType<FormRules>(Object),
   },
   /**
-   * @description Position of label. If set to `'left'` or `'right'`, `label-width` prop is also required.
-   */
-  labelPosition: {
-    type: String,
-    values: ['left', 'right', 'top'],
-    default: 'right',
-  },
-  /**
-   * @description Position of asterisk.
-   */
-  requireAsteriskPosition: {
-    type: String,
-    values: ['left', 'right'],
-    default: 'left',
-  },
-  /**
-   * @description Width of label, e.g. `'50px'`. All its direct child form items will inherit this value. `auto` is supported.
-   */
-  labelWidth: {
-    type: [String, Number],
-    default: '',
-  },
-  /**
-   * @description Suffix of the label.
-   */
-  labelSuffix: {
-    type: String,
-    default: '',
-  },
-  /**
-   * @description Whether the form is inline.
-   */
-  inline: Boolean,
-  /**
-   * @description Whether to display the error message inline with the form item.
-   */
-  inlineMessage: Boolean,
-  /**
-   * @description Whether to display an icon indicating the validation result.
-   */
-  statusIcon: Boolean,
-  /**
    * @description Whether to show the error message.
    */
-  showMessage: {
-    type: Boolean,
-    default: true,
-  },
   /**
    * @description Whether to trigger validation when the `rules` prop is changed.
    */
@@ -93,10 +39,6 @@ export const formProps = buildProps({
     type: Boolean,
     default: true,
   },
-  /**
-   * @description Whether to hide required fields should have a red asterisk (star) beside their labels.
-   */
-  hideRequiredAsterisk: Boolean,
   /**
    * @description When validation fails, scroll to the first error form entry.
    */
