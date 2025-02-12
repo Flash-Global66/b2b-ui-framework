3818084f11c77b9971446a0dcc1c0490bfff09d4
Andersson Mesa <andersson.mesa@global66.com>
2025-02-12 13:28:53 -0500

    avance form

--- components/Form/index.ts
+++ components/Form/index.ts
@@ -1,10 +1,21 @@
-import GForm from "./Form.vue";
-export type { 
-    FormRules as GFormRules,
-    FormValidationResult as GFormValidationResult, 
-    FormValidateCallback as GFormValidateCallback,
-} from 'element-plus';
+import { withInstall, withNoopInstall, SFCWithInstall } from 'element-plus/es/utils/index.mjs'
+import From from './Form.vue'
+import FormItem from './form-item.vue'
 
-export type GFormInstance = InstanceType<typeof GForm>
+export const GForm: SFCWithInstall<typeof From> & {
+  FormItem: typeof FormItem
+} = withInstall(From, {
+  FormItem,
+})
+export default GForm
+export const GFormItem: SFCWithInstall<typeof FormItem> =
+  withNoopInstall(FormItem)
 
-export { GForm };
+export * from './form'
+export * from './form-item'
+export * from './types'
+export * from './constants'
+export * from './hooks'
+
+export type FormInstance = InstanceType<typeof From>
+export type FormItemInstance = InstanceType<typeof FormItem>
