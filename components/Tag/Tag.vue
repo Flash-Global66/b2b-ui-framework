4c7ba199d56ca79a3a974771d039b746ecb4608b
Joan Rojas <joan.rojas@global66.com>
2025-03-10 17:40:51 -0500

    feat: tag finished

--- components/Tag/Tag.vue
+++ components/Tag/Tag.vue
@@ -1,18 +1,26 @@
 <template>
-  <span
-    v-if="disableTransitions"
-    :class="containerKls"
-    :style="{ backgroundColor: color }"
-    @click="handleClick"
-  >
+  <span v-if="disableTransitions" :class="containerKls" @click="handleClick">
+    <slot v-if="$slots.prefix || prefixIcon" name="prefix">
+      <span v-if="prefixIcon" :class="ns.em(size, 'prefix-icon')">
+        <g-icon-font :name="prefixIcon" />
+      </span>
+    </slot>
     <span :class="ns.e('content')">
-      <slot>
-        {{ text }}
-      </slot>
+      <span v-if="text">{{ text }}</span>
+      <slot v-else />
     </span>
-    <div v-if="closable" :class="ns.e('close')" @click.stop="handleClose">
-      <icon name="cross" />
-    </div>
+    <slot name="suffix" v-if="$slots.suffix || suffixIcon || closable">
+      <span v-if="suffixIcon && !closable" :class="ns.em(size, 'suffix-icon')">
+        <g-icon-font :name="suffixIcon" />
+      </span>
+      <span
+        v-else-if="closable"
+        @click.stop="handleClose"
+        :class="[ns.em(size, 'suffix-icon'), ns.e('close')]"
+      >
+        <g-icon-font name="regular times" />
+      </span>
+    </slot>
   </span>
   <transition
     v-else
@@ -20,49 +28,54 @@
     appear
     @vue:mounted="handleVNodeMounted"
   >
-    <span
-      :class="containerKls"
-      :style="{ backgroundColor: color }"
-      @click="handleClick"
-    >
+    <span :class="containerKls" @click="handleClick">
+      <slot v-if="$slots.prefix || prefixIcon" name="prefix">
+        <span v-if="prefixIcon" :class="ns.em(size, 'prefix-icon')">
+          <g-icon-font :name="prefixIcon" />
+        </span>
+      </slot>
       <span :class="ns.e('content')">
-        <slot>
-          {{ text }}
-        </slot>
+        <span v-if="text">{{ text }}</span>
+        <slot v-else />
       </span>
-      <div v-if="closable" :class="ns.e('close')" @click.stop="handleClose">
-        <icon name="cross" />
-      </div>
+      <slot name="suffix" v-if="$slots.suffix || suffixIcon || closable">
+        <span v-if="suffixIcon && !closable" :class="ns.em(size, 'suffix-icon')">
+          <g-icon-font :name="suffixIcon" />
+        </span>
+        <span
+          v-else-if="closable"
+          @click.stop="handleClose"
+          :class="[ns.em(size, 'suffix-icon'), ns.e('close')]"
+        >
+          <g-icon-font name="regular times" />
+        </span>
+      </slot>
     </span>
   </transition>
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue'
-
+import { useNamespace } from 'element-plus/es/hooks/index'
+import { GIconFont } from '@flash-global66/b2b-ui-icon-font'
 import { tagEmits, tagProps } from './tag'
 import type { VNode } from 'vue'
-import { useFormSize, useNamespace } from 'element-plus'
-import { Icon } from '@flash-global66/b2b-ui-icon'
 
 defineOptions({
-  name: 'Tag',
+  name: 'GTag'
 })
 const props = defineProps(tagProps)
 const emit = defineEmits(tagEmits)
 
-const tagSize = useFormSize()
 const ns = useNamespace('tag')
 const containerKls = computed(() => {
-  const { type, hit, effect, closable, round } = props
+  const { type, effect, closable, size } = props
   return [
     ns.b(),
     ns.is('closable', closable),
-    ns.m(type || 'primary'),
-    ns.m(tagSize.value),
-    ns.m(effect),
-    ns.is('hit', hit),
-    ns.is('round', round),
+    ns.e(type || 'grey'),
+    ns.e(size),
+    ns.em(effect, type || 'grey')
   ]
 })
 
