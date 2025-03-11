a328e3fef24fb26955647984e07267e74c80655c
Joan Rojas <joan.rojas@global66.com>
2025-03-11 11:12:09 -0500

    feat: corrections UX and change index dialog

--- components/Dialog/index.ts
+++ components/Dialog/index.ts
@@ -3,9 +3,11 @@
  * Derived from Element Plus v2.9.5
  * https://github.com/element-plus/element-plus/tree/2.9.5/packages/components/dialog
  */
-import GDialog from "./Dialog.vue";
-import type { FooterButton } from './types';
+import GDialog from './Dialog.vue'
+import { useDialog } from './use-dialog'
+import { DialogEmits, DialogProps } from './dialog'
+import type { FooterButton } from './types'
 
-export { GDialog };
-
-export type { FooterButton };
+export default { GDialog }
+export { useDialog, DialogEmits, DialogProps }
+export type { FooterButton }
