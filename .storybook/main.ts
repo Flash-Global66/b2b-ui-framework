f2aac68dcebd64bdefeda664b58c6aa28d685376
Joan Rojas <joan.rojas@global66.com>
2025-03-17 16:35:31 -0500

    feat: corrections

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ const config: StorybookConfig = {
     name: "@storybook/vue3-vite",
     options: {}
   },
-  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
 
   addons: [
     "@storybook/addon-links",
