2a28458e5e75917f9131e849c2ca3d53245c5aa5
Joan Rojas <joan.rojas@global66.com>
2025-03-19 10:21:27 -0500

    feat: advance #1

---
+++ components/tooltip/src/index.ts
@@ -0,0 +1,10 @@
+import TooltipV2 from './tooltip.vue'
+
+export * from './arrow'
+export * from './content'
+export * from './root'
+export * from './tooltip'
+export * from './trigger'
+export * from './constants'
+
+export default TooltipV2
